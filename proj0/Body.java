6d5e898c50f289a32e642666b51415892068fd8f
Anqi Ni <angelo23@squirtle.cs.uchicago.edu>
2019-11-05 17:23:14 -0600

    modify body

--- proj0/Body.java
+++ proj0/Body.java
@@ -25,5 +25,52 @@ public class Body {
 		mass = b.mass;
 		imgFileName = b.imgFileName;
 	}
+	/** calculate distance */
+	public double calcDistance(Body b) {
+		double dx = b.xxPos - this.xxPos;
+		double dy = b.yyPos - this.yyPos;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	/** calculate force */
+	public double calcForceExertedBy(Body b) {
+		double r = this.calcDistance(b);
+		double G = 6.67 * Math.pow(10, -11);
+		return G * this.mass * b.mass / (r * r);
+	}
+
+	/** calcForceExertedByX and calcForceExertedByY */
+	public double calcForceExertedByX(Body b) {
+		double force = this.calcForceExertedBy(b);
+		double r = this.calcDistance(b);
+		return force * (b.xxPos - this.xxPos) / r;
+	}
+
+	/** calcForceExertedByX and calcForceExertedByY */
+	public double calcForceExertedByY(Body b) {
+		double force = this.calcForceExertedBy(b);
+		double r = this.calcDistance(b);
+		return force * (b.yyPos - this.yyPos) / r;
+	}
+
+	/** calcNetForceExertedByX and calcNetForceExertedByY */
+	public double calcNetForceExertedByX(Body[] arr) {
+		double res = 0;
+		arr.remove(this);
+		for (Body b: arr) {
+			res += this.calcForceExertedByX(b);
+		}
+		return res;
+	}
+
+	/** calcNetForceExertedByX and calcNetForceExertedByY */
+	public double calcNetForceExertedByY(Body[] arr) {
+		double res = 0;
+		arr.remove(this);
+		for (Body b: arr) {
+			res += this.calcForceExertedByY(b);
+		}
+		return res;
+	}
 
 }
